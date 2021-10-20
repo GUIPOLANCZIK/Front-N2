dcc63e4485a99ae08b3c1cd94927fcb8e1416d10
guilherme polanczik <gui.lherme_rp@hotmail.com>
2021-10-20 20:36:54 -0300

    front da N2

---
+++ js/funcoes.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+    document.getElementById("cortina").style.backgroundColor = "rgba(209,8 , 8,0.3)";
+    document.getElementById("modal").style.marginTop = "120px";
+
+}
+function fecharModal() {
+    document.getElementById("cortina").style.backgroundColor = "rgba(0,0,0,0)";
+    document.getElementById("modal").style.marginTop = "-500px";
+}
